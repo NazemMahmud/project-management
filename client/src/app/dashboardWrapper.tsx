ad3ef274c0e93ad89913ae481a5c67c0de0b8655
Piash <nazem.piash10@gmail.com>
2024-11-26 03:27:36 +0600

    commit 2: client: 1. add images inside public folder 2. Update: next config for image path 3. Update: main app layout 4. Create: navbar component init 5. Create: dashboard wrapper init 6. Update: tsconfig to get base path as src directory instead of app

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Navbar from "@/components/Navbar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/*<Sidebar />*/}
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg  md:pl-64`}
+      >
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+        <DashboardLayout>{children}</DashboardLayout>
+  );
+};
+
+export default DashboardWrapper;
